6c7e98f8e76324954111abe644a657b3b4a9229d
Arturo Castro Prieto <me@arturocastro.net>
2024-09-17 10:51:26 +0200

    copy rust lib to correct dir

--- src/build.sh
+++ src/build.sh
@@ -2,7 +2,7 @@ echo "\033[32mbuild native plugins\033[m"
 cd `dirname $0`
 cd native
 cargo build --target x86_64-pc-windows-gnu --release
-cp target/x86_64-pc-windows-gnu/release/vl_openexr_native.dll ../../lib/native/VL.OpenEXR.Native.dll
+cp target/x86_64-pc-windows-gnu/release/vl_openexr_native.dll ../../runtimes/win-x64/native/VL.OpenEXR.Native.dll
 echo ""
 
 echo "\033[32mbuild managed plugins\033[m"
